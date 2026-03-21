66304cb28431512fc8576979fb6f60a91433689e
deevfer <vasquez.fernando94@gmail.com>
2026-03-20 21:18:41 -0600

    añadiendo android/cors

--- config/cors.php
+++ config/cors.php
@@ -9,6 +9,8 @@ return [
         'https://sona.fernandovasquez.tech',
         'http://localhost:5181',
         'http://127.0.0.1:8000',
+        'http://localhost',
+        'capacitor://localhost',
     ],
     'allowed_headers' => ['*'],
     'exposed_headers' => ['Retry-After'],
